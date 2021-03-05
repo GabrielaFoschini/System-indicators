2c2b953d454f195896e73681f597b0ff91241a14
Gabriela Foschini <gabriela.foschini@codeby.com.br>
2021-03-05 15:45:58 -0300

    fixing bugs

--- config/database.js
+++ config/database.js
@@ -71,11 +71,11 @@ module.exports = {
   pg: {
     client: 'pg',
     connection: {
-      host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
+      host: Env.get('DB_HOST', 'ec2-54-89-49-242.compute-1.amazonaws.com'),
+      port: Env.get('DB_PORT', '5432'),
+      user: Env.get('DB_USER', 'zaqwieezyihkbv'),
+      password: Env.get('DB_PASSWORD', '9c29b6df94e3c9bcc3e5ecd59dc8b1bbc80031085b78f75cf169221d0f64ad72'),
+      database: Env.get('DB_DATABASE', 'dbh44kgb071in0')
     }
   }
 }
